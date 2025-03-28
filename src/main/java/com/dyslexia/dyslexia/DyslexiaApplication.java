748d39061f50e403c6ad234ab5392590f13c49bb
west_east <west.east1832@gmail.com>
2025-03-28 15:31:40 +0900

    init

---
+++ src/main/java/com/dyslexia/dyslexia/DyslexiaApplication.java
@@ -0,0 +1,13 @@
+package com.dyslexia.dyslexia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyslexiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyslexiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyslexia/dyslexia/DyslexiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyslexia.dyslexia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyslexiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
