b1549d27089a719f9b3708b050e00617ed8bc4e2
west_east <west.east1832@gmail.com>
2025-04-12 14:26:41 +0900

    feat: kakao oauth

---
+++ src/main/java/com/dyslexia/dyslexia/dto/AuthResponseDto.java
@@ -0,0 +1,19 @@
+package com.dyslexia.dyslexia.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDto {
+    private boolean registered;
+    private String clientId;
+    private String nickname;
+    private String userType;
+    private String accessToken;
+    private String refreshToken;
+}
